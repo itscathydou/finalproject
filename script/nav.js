56b6e3de061ae818a780b5a28a495775da7c70ed
itscathydou <catherine.lu@has.hc.edu.tw>
2017-06-14 16:05:25 +0800

    Initial commit

--- script/nav.js
+++ script/nav.js
@@ -1,12 +1,18 @@
 $(document).ready(function(){
     $("#drone_menu").click(function() {
-    $("#dronedrop").fadeToggle();
+        $("#dronedrop").fadeToggle();
+        $("#dslrdrop").css("display","none");
+        $("#actiondrop").css("display","none");
     });
     $('#dslr_menu').click(function() {
         $("#dslrdrop").fadeToggle();
+        $("#dronedrop").css("display","none");
+        $("#actiondrop").css("display","none");
     });
     $('#action_menu').click(function() {
         $("#actiondrop").fadeToggle();
+        $("#dslrdrop").css("display","none");
+        $("#dronedrop").css("display","none");
     });
     $('#downarrow').click(function(){
         $('body').animate({scrollTop:'+=800px'},800);
